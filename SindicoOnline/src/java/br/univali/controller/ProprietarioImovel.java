d2052b00c0a685b4b7e9f05f5dbdb359a8d07fd0
jader <jader@10.6.24.32>
2014-06-04 20:46:15 -0300

    Adicionando itens do projeto. Parte da API Rest usando Jersey;

---
+++ SindicoOnline/src/java/br/univali/controller/ProprietarioImovel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.univali.controller;
+
+/**
+ *
+ * @author jader
+ */
+public class ProprietarioImovel extends Pessoa {
+    private int idProprietarioImovel;
+
+    /**
+     * @return the idProprietarioImovel
+     */
+    public int getIdProprietarioImovel() {
+        return idProprietarioImovel;
+    }
+
+    /**
+     * @param idProprietarioImovel the idProprietarioImovel to set
+     */
+    public void setIdProprietarioImovel(int idProprietarioImovel) {
+        this.idProprietarioImovel = idProprietarioImovel;
+    }
+    
+}
