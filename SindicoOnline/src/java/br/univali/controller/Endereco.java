3ab6868aafe57de3b70ea116338d706ce76d779f
jader <jader@10.6.24.32>
2014-06-04 19:59:55 -0300

    Adicionando as classes endereço e pessoa

---
+++ SindicoOnline/src/java/br/univali/controller/Endereco.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.univali.controller;
+
+/**
+ *
+ * @author jader
+ */
+public class Endereco {
+    private String estado;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String complemento;
+    private String pais;
+    private int idEndereco;
+    private String rua;
+    private int numero;
+    
+}
---
+++ SindicoOnline/src/java/br/univali/controller/Pessoa.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.univali.controller;
+
+/**
+ *
+ * @author jader
+ */
+public class Pessoa {
+    private String celular;
+    private String cpf;
+    private String dataNascimento;
+    private String foneResidencial;
+    private String nome;
+    private int rg;
+    private String sexo;
+    private String usuario;
+    private String senha;
+    private int idPessoa;
+    private boolean ehSindico;
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the foneResidencial
+     */
+    public String getFoneResidencial() {
+        return foneResidencial;
+    }
+
+    /**
+     * @param foneResidencial the foneResidencial to set
+     */
+    public void setFoneResidencial(String foneResidencial) {
+        this.foneResidencial = foneResidencial;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the rg
+     */
+    public int getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the idPessoa
+     */
+    public int getIdPessoa() {
+        return idPessoa;
+    }
+
+    /**
+     * @param idPessoa the idPessoa to set
+     */
+    public void setIdPessoa(int idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    /**
+     * @return the ehSindico
+     */
+    public boolean isEhSindico() {
+        return ehSindico;
+    }
+
+    /**
+     * @param ehSindico the ehSindico to set
+     */
+    public void setEhSindico(boolean ehSindico) {
+        this.ehSindico = ehSindico;
+    }
+    
+    
+    
+    
+}
