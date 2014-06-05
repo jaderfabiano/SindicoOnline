405ce6f3a70214883c4cdc0d095476f0e6b93522
jader <jader@10.6.24.32>
2014-06-04 21:27:10 -0300

    Atualizando recurso morador

--- SindicoOnline/src/java/br/univali/resource/MoradorResource.java
+++ SindicoOnline/src/java/br/univali/resource/MoradorResource.java
@@ -6,15 +6,16 @@
 
 package br.univali.resource;
 
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
+
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * REST Web Service
@@ -56,8 +57,11 @@ public class MoradorResource {
     
     @POST
     @Consumes("application/json")
-    public void novoMorador(String content) {
-        System.out.println("Chegou comando post");
+    public String novoMorador(String track) {
+        
+       
+        System.out.println("Chegou comando post " + track);
+        return "OK";
         
     }
 }
